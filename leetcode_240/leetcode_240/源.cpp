d48d6c79d4fe95f2f4cb900a7389f1394316e0a3
1029458906@qq.com <1029458906@qq.com>
2018-07-19 16:22:09 +0800

    240: 优化 利用矩阵性质直接查找 速度最快的是直接从第一行开始查找，大于了就看下一行 WTF!???

--- leetcode_240/leetcode_240/源.cpp
+++ leetcode_240/leetcode_240/源.cpp
@@ -5,78 +5,18 @@
 #define ROWSIZE 5
 #define COLSIZE 5
 
-typedef struct point_t {
-	int x, y, value;
-	struct point_t *next;
-}point_t, *point;
+bool searchMatrix(int** arr, int rows, int cols, int target) {
+	int r = rows - 1;
+	int c = 0;
 
-point newPoint(int x, int y, int value)
-{
-	point temp = (point)malloc(sizeof(point_t));
-	temp->x = x;
-	temp->y = y;
-	temp->value = value;
-	temp->next = NULL;
-	return temp;
-}
-
-void insert(point *tail, point node)
-{
-	(*tail)->next = node;
-	(*tail) = node;
-}
-
-point get(point head, point *tail)
-{
-	point temp = head->next;
-	if (temp == NULL) {
-		return NULL;
-	}
-	head->next = head->next->next;
-	if (temp == *tail) {
-		*tail = head;
-	}
-	return temp;
-}
-
-bool searchMatrix(int** matrix, int matrixRowSize, int matrixColSize, int target) {
-	if (matrixRowSize == 0 || matrixColSize == 0) {
-		return false;
-	}
-	if (target == matrix[0][0] || target == matrix[matrixRowSize - 1][matrixColSize - 1]) {
-		return true;
-	}
-	int x, y;
-	x = matrixRowSize / 2;
-	y = matrixColSize / 2;
-	point_t head;
-	head.next = newPoint(x, y, matrix[x][y]);
-	point tail = head.next;
-	point now;
-	while ((now = get(&head, &tail)) != NULL) {
-		if (target == now->value) {
+	while (r >= 0 && c < cols)
+	{
+		if (arr[r][c] == target)
 			return true;
-		}
-		else if (target > now->value) {
-			if (now->x + 1 < matrixRowSize && matrix[now->x + 1][now->y] != INT_MAX) {
-				insert(&tail, newPoint(now->x + 1, now->y, matrix[now->x + 1][now->y]));
-				matrix[now->x + 1][now->y] = INT_MAX;
-			}
-			if (now->y + 1 < matrixColSize && matrix[now->x][now->y + 1] != INT_MAX) {
-				insert(&tail, newPoint(now->x, now->y + 1, matrix[now->x][now->y + 1]));
-				matrix[now->x][now->y + 1] = INT_MAX;
-			}
-		}
-		else {
-			if (now->x >= 1 && matrix[now->x - 1][now->y] != INT_MAX) {
-				insert(&tail, newPoint(now->x - 1, now->y, matrix[now->x - 1][now->y]));
-				matrix[now->x - 1][now->y] = INT_MAX;
-			}
-			if (now->y >= 1 && matrix[now->x][now->y - 1] != INT_MAX) {
-				insert(&tail, newPoint(now->x, now->y - 1, matrix[now->x][now->y - 1]));
-				matrix[now->x][now->y - 1] = INT_MAX;
-			}
-		}
+		if (target > arr[r][c])
+			c++;
+		else
+			r--;
 	}
 	return false;
 }
